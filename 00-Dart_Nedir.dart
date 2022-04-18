cd5690e8bdb2e0b2f1b80de67defb458a1341382
erdincDonmez <erdinc_donmez@yahoo.com>
2022-04-18 08:26:06 +0000

    Açıklama eklendi.

---
+++ 00-Dart_Nedir.dart
@@ -0,0 +1,14 @@
+/*
+  > DART NEDİR?
+  
+  > Dart java ve C benzeri yazılımı olan bir dildir.
+  > Nesne tabanlı bir dildir.
+  > Büyük küçük harfe duyarlı.
+  > Google tarafından flutter için kullanılan program dilidir.
+  > Dart kodlarını yazmak için vscode uygulamasını kullanabilirsiniz. 
+    > vscode uygulamasında ilgili plugin yüklü değilse -ki muhtemelen yüklü değildir- önce onu yükle.
+  > online kod yazmak için https://replit.com/ veya https://dartpad.dev/ adresini kullanabilirsin.
+  > 
+  > 
+  > 
+*/
